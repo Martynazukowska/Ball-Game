a5d2ad4c8c6b133fdf311c391f1806aaa27ab7a5
Dawid-Lukasiewicz <lukasiewicz@onet.com.pl>
2022-06-07 00:58:32 +0200

    flash read, write and debugger no reset configuration

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -108,6 +108,8 @@ static void MX_TIM6_Init(void);
 static void zmien_na_char(int pom,int x, int y);
 static void Generate_Obstacles(ObstacleDef *obstacles, uint8_t NumberOfObjects, uint16_t width_limit, uint16_t height_limit , uint16_t gap);
 static void Generate_Item(Item *point, uint8_t NumberOfPoints, uint16_t width, uint16_t height , uint16_t gap);
+static void scoreFlashWrite(uint16_t Score);
+static uint16_t scoreFlashRead();
 /* USER CODE END 0 */
 
 /**
@@ -170,7 +172,8 @@ int main(void)
   uint16_t width = 10;
   uint16_t height= 10;
 
-  uint16_t gap = 60;
+//  uint16_t gap = 60;
+  uint16_t gap = scoreFlashRead();
   Generate_Obstacles(obstacles, OBSTACLES_NUMBER, width_limit, height_limit , gap);
   Generate_Item(point, POINTS_NUMBER, width, height, gap);
 
@@ -309,10 +312,6 @@ int main(void)
 		  tryb=5;
 		  break;
 	  case 5:
-		  if(punkty>=MAX_PUNKTOW)
-		  {
-			  MAX_PUNKTOW=punkty;
-		  }
 		  BSP_LCD_DisplayStringAt(0,BSP_LCD_GetYSize()/2-120, (uint8_t*)"---------------",LEFT_MODE);
 		  BSP_LCD_DisplayStringAt(0,BSP_LCD_GetYSize()/2-100, (uint8_t*)"Twoj Wynik",CENTER_MODE);
 		  BSP_LCD_DisplayStringAt(0,BSP_LCD_GetYSize()/2-79, (uint8_t*)"---------------",LEFT_MODE);
@@ -324,6 +323,11 @@ int main(void)
 		  BSP_LCD_DisplayStringAt(0,BSP_LCD_GetYSize()/2+61, (uint8_t*)"---------------",LEFT_MODE);
 
 		  zmien_na_char(MAX_PUNKTOW,BSP_LCD_GetXSize()/2-20,BSP_LCD_GetYSize()/2+95);
+
+		  if(punkty>=MAX_PUNKTOW)
+		  {
+			  MAX_PUNKTOW=punkty;
+		  }
 
 		  ReloadFlag = 0;
 		  BSP_LCD_Relaod(LCD_RELOAD_VERTICAL_BLANKING);
@@ -910,165 +914,35 @@ void Generate_Item(Item *point, uint8_t NumberOfPointss, uint16_t width_limit, u
 	}
 }
 
-/**
-  * @brief LCD Configuration.
-  * @note  This function Configure the LTDC peripheral :
-  *        1) Configure the Pixel Clock for the LCD
-  *        2) Configure the LTDC Timing and Polarity
-  *        3) Configure the LTDC Layer 1 :
-  *           - direct color (RGB565) as pixel format
-  *           - The frame buffer is located at FLASH memory
-  *           - The Layer size configuration : 240x160
-  *        4) Configure the LTDC Layer 2 :
-  *           - direct color (RGB565) as pixel format
-  *           - The frame buffer is located at FLASH memory
-  *           - The Layer size configuration : 240x160
-  * @retval
-  *  None
-  */
-//static void LCD_Config(void)
-//{
-//  LTDC_LayerCfgTypeDef pLayerCfg;
-//  LTDC_LayerCfgTypeDef pLayerCfg1;
-//
-//  /* Initialization of ILI9341 component*/
-//  ili9341_Init();
-//
-///* LTDC Initialization -------------------------------------------------------*/
-//
-//  /* Polarity configuration */
-//  /* Initialize the horizontal synchronization polarity as active low */
-//  LtdcHandle.Init.HSPolarity = LTDC_HSPOLARITY_AL;
-//  /* Initialize the vertical synchronization polarity as active low */
-//  LtdcHandle.Init.VSPolarity = LTDC_VSPOLARITY_AL;
-//  /* Initialize the data enable polarity as active low */
-//  LtdcHandle.Init.DEPolarity = LTDC_DEPOLARITY_AL;
-//  /* Initialize the pixel clock polarity as input pixel clock */
-//  LtdcHandle.Init.PCPolarity = LTDC_PCPOLARITY_IPC;
-//
-//  /* Timing configuration  (Typical configuration from ILI9341 datasheet)
-//      HSYNC=10 (9+1)
-//      HBP=20 (29-10+1)
-//      ActiveW=240 (269-20-10+1)
-//      HFP=10 (279-240-20-10+1)
-//
-//      VSYNC=2 (1+1)
-//      VBP=2 (3-2+1)
-//      ActiveH=320 (323-2-2+1)
-//      VFP=4 (327-320-2-2+1)
-//  */
-//
-//  /* Timing configuration */
-//  /* Horizontal synchronization width = Hsync - 1 */
-//  LtdcHandle.Init.HorizontalSync = 9;
-//  /* Vertical synchronization height = Vsync - 1 */
-//  LtdcHandle.Init.VerticalSync = 1;
-//  /* Accumulated horizontal back porch = Hsync + HBP - 1 */
-//  LtdcHandle.Init.AccumulatedHBP = 29;
-//  /* Accumulated vertical back porch = Vsync + VBP - 1 */
-//  LtdcHandle.Init.AccumulatedVBP = 3;
-//  /* Accumulated active width = Hsync + HBP + Active Width - 1 */
-//  LtdcHandle.Init.AccumulatedActiveH = 323;
-//  /* Accumulated active height = Vsync + VBP + Active Heigh - 1 */
-//  LtdcHandle.Init.AccumulatedActiveW = 269;
-//  /* Total height = Vsync + VBP + Active Heigh + VFP - 1 */
-//  LtdcHandle.Init.TotalHeigh = 327;
-//  /* Total width = Hsync + HBP + Active Width + HFP - 1 */
-//  LtdcHandle.Init.TotalWidth = 279;
-//
-//  /* Configure R,G,B component values for LCD background color */
-//  LtdcHandle.Init.Backcolor.Blue = 0;
-//  LtdcHandle.Init.Backcolor.Green = 0;
-//  LtdcHandle.Init.Backcolor.Red = 0;
-//
-//  LtdcHandle.Instance = LTDC;
-//
-///* Layer1 Configuration ------------------------------------------------------*/
-//
-//  /* Windowing configuration */
-//  pLayerCfg.WindowX0 = 0;
-//  pLayerCfg.WindowX1 = BSP_LCD_GetXSize();
-//  pLayerCfg.WindowY0 = 0;
-//  pLayerCfg.WindowY1 = BSP_LCD_GetYSize();
-//
-//  /* Pixel Format configuration*/
-//
-////  pLayerCfg.PixelFormat = LTDC_PIXEL_FORMAT_RGB565;
-//  pLayerCfg.PixelFormat = LTDC_PIXEL_FORMAT_ARGB8888;
-//  /* Start Address configuration : frame buffer is located at FLASH memory */
-//  //pLayerCfg.FBStartAdress = (uint32_t)&ST_LOGO_1;
-//  pLayerCfg.FBStartAdress=0xD0000000;
-//
-//  /* Alpha constant (255 totally opaque) */
-//  pLayerCfg.Alpha = 255;
-//
-//  /* Default Color configuration (configure A,R,G,B component values) */
-//  pLayerCfg.Alpha0 = 0;
-//  pLayerCfg.Backcolor.Blue = 0;
-//  pLayerCfg.Backcolor.Green = 0;
-//  pLayerCfg.Backcolor.Red = 0;
-//
-//  /* Configure blending factors */
-//  pLayerCfg.BlendingFactor1 = LTDC_BLENDING_FACTOR1_PAxCA;
-//  pLayerCfg.BlendingFactor2 = LTDC_BLENDING_FACTOR2_PAxCA;
-//
-//  /* Configure the number of lines and number of pixels per line */
-//  pLayerCfg.ImageWidth = BSP_LCD_GetXSize();
-//  pLayerCfg.ImageHeight = BSP_LCD_GetYSize() ;
-//
-///* Layer2 Configuration ------------------------------------------------------*/
-//
-//  /* Windowing configuration */
-//  pLayerCfg1.WindowX0 = 0;
-//  pLayerCfg1.WindowX1 = BSP_LCD_GetXSize();
-//  pLayerCfg1.WindowY0 = 0;
-//  pLayerCfg1.WindowY1 = BSP_LCD_GetYSize();
-//
-//  /* Pixel Format configuration*/
-////  pLayerCfg1.PixelFormat = LTDC_PIXEL_FORMAT_RGB565;
-//  pLayerCfg1.PixelFormat =LTDC_PIXEL_FORMAT_ARGB8888;
-//  /* Start Address configuration : frame buffer is located at FLASH memory */
-// // pLayerCfg1.FBStartAdress = (uint32_t)&ST_LOGO_2;
-//  pLayerCfg1.FBStartAdress =0xD0000000;
-//
-//  /* Alpha constant (255 totally opaque) */
-//  pLayerCfg1.Alpha = 200;
-//
-//  /* Default Color configuration (configure A,R,G,B component values) */
-//  pLayerCfg1.Alpha0 = 0;
-//  pLayerCfg1.Backcolor.Blue = 0;
-//  pLayerCfg1.Backcolor.Green = 0;
-//  pLayerCfg1.Backcolor.Red = 0;
-//
-////  /* Configure blending factors */
-//  pLayerCfg1.BlendingFactor1 = LTDC_BLENDING_FACTOR1_PAxCA;
-//  pLayerCfg1.BlendingFactor2 = LTDC_BLENDING_FACTOR2_PAxCA;
-//
-//  /* Configure the number of lines and number of pixels per line */
-//  pLayerCfg1.ImageWidth = BSP_LCD_GetXSize();
-//  pLayerCfg1.ImageHeight = BSP_LCD_GetYSize();
-//
-//  /* Configure the LTDC */
-//  if(HAL_LTDC_Init(&LtdcHandle) != HAL_OK)
-//  {
-//    /* Initialization Error */
-//    Error_Handler();
-//  }
-//
-//  /* Configure the Background Layer*/
-//  if(HAL_LTDC_ConfigLayer(&LtdcHandle, &pLayerCfg, 0) != HAL_OK)
-//  {
-//    /* Initialization Error */
-//    Error_Handler();
-//  }
-//
-//  /* Configure the Foreground Layer*/
-//  if(HAL_LTDC_ConfigLayer(&LtdcHandle, &pLayerCfg1, 1) != HAL_OK)
-//  {
-//    /* Initialization Error */
-//    Error_Handler();
-//  }
-//}
+void scoreFlashWrite(uint16_t Score)
+{
+	uint32_t SECTORError;
+	FLASH_EraseInitTypeDef eraseStruct;
+	eraseStruct.TypeErase = FLASH_TYPEERASE_SECTORS;   /*!< Mass erase or sector Erase.*/
+	eraseStruct.Sector = 	FLASH_SECTOR_20;      /*!< Initial FLASH sector to erase when Mass erase is disabled*/
+	eraseStruct.NbSectors = 1;   /*!< Number of sectors to be erased.*/
+	eraseStruct.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+
+	HAL_FLASH_Unlock();
+	if(HAL_FLASHEx_Erase(&eraseStruct, &SECTORError) != HAL_OK)
+	{
+		BSP_LCD_DisplayStringAt(0,BSP_LCD_GetYSize()/2-30, (uint8_t*)"ERROR REMOVING PREVIOUS SCORE",CENTER_MODE);
+		return;
+	}
+
+	if(HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, FLASH_SECTOR_20, (uint64_t)Score) != HAL_OK)
+	{
+		BSP_LCD_DisplayStringAt(0,BSP_LCD_GetYSize()/2-30, (uint8_t*)"ERROR SAVING NEW SCORE",CENTER_MODE);
+		return;
+	}
+	HAL_FLASH_Lock();
+}
+
+uint16_t scoreFlashRead()
+{
+	uint16_t returnScore = *(uint16_t*)FLASH_SECTOR_20;
+	return returnScore;
+}
 
 /**
   * @brief  Reload Event callback.
